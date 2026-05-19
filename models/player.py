dd40a21c18f3f06d9fa34acfa77894b1f1a00640
Ignat22 <147313268+Ignat22@users.noreply.github.com>
2026-05-20 00:29:38 +0500

    fix: rename atribute, add __str__ method

--- models/player.py
+++ models/player.py
@@ -25,3 +25,26 @@ class GameSession:
         self.player_name = player_name
         self.score = score
         self.date = date
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.scores = []
+
+    def add_score(self, score):
+        self.scores.append(score)
+
+    def get_scores(self):
+        return self.scores
+
+    def __str__(self):
+        return f"Player: {self.name}"
+
+
+class GameSession:
+    def __init__(self, player_name, score, date):
+        self.player_name = player_name
+        self.score = score
+        self.date = date
+
+    def __str__(self):
+        return f"{self.player_name} - {self.score} - {self.date}"
