21fa59288d36aa4ca6c1a53ab096c8bcba040bfb
Ignat22 <147313268+Ignat22@users.noreply.github.com>
2026-05-20 00:34:03 +0500

    add BaseEntity parent class

--- models/player.py
+++ models/player.py
@@ -45,6 +45,46 @@ class GameSession:
         self.player_name = player_name
         self.score = score
         self.date = date
+class BaseEntity:
+    def __init__(self, name):
+        self.name = name
 
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Player(BaseEntity):
+    def __init__(self, name):
+        super().__init__(name)
+        self._scores = []
+
+    def add_score(self, score):
+        self._scores.append(score)
+
+    def get_scores(self):
+        return self._scores
+
+    def __str__(self):
+        return f"Player: {self.name}, games played: {len(self._scores)}"
+
+
+class GameSession(BaseEntity):
+    def __init__(self, player_name, score, date):
+        super().__init__(player_name)
+        self.score = score
+        self.date = date
+
+    def to_dict(self):
+        return {
+            "player": self.name,
+            "score": self.score,
+            "date": self.date
+        }
+
+    def __str__(self):
+        return f"{self.name} scored {self.score} on {self.date}"
     def __str__(self):
         return f"{self.player_name} - {self.score} - {self.date}"
