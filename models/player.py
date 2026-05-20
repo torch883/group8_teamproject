c46fa83efe983f0f7c7221107a3c3243b7918b59
Ignat22 <147313268+Ignat22@users.noreply.github.com>
2026-05-20 10:29:11 +0500

    Update player.py

--- models/player.py
+++ models/player.py
@@ -1,40 +1,3 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.scores = []
-
-    def add_score(self, score):
-        self.scores.append(score)
-
-    def get_scores(self):
-        return self.scores
-
-
-class GameSession:
-    def __init__(self, player_name, score, date):
-        self.player_name = player_name
-        self.score = score
-        self.date = date
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.scores = []
-
-    def add_score(self, score):
-        self.scores.append(score)
-
-    def get_scores(self):
-        return self.scores
-
-    def __str__(self):
-        return f"Player: {self.name}"
-
-
-class GameSession:
-    def __init__(self, player_name, score, date):
-        self.player_name = player_name
-        self.score = score
-        self.date = date
 class BaseEntity:
     def __init__(self, name):
         self.name = name
@@ -76,5 +39,3 @@ class GameSession(BaseEntity):
 
     def __str__(self):
         return f"{self.name} scored {self.score} on {self.date}"
-    def __str__(self):
-        return f"{self.player_name} - {self.score} - {self.date}"
