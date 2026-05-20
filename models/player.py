a54861b4c0f5ea7d1ff62605bf84ed9cf3aba777
Ignat22 <147313268+Ignat22@users.noreply.github.com>
2026-05-20 10:27:23 +0500

    Update player.py

--- models/player.py
+++ models/player.py
@@ -8,16 +8,6 @@ class Player:
 
     def get_scores(self):
         return self.scores
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.scores = []
-
-    def add_score(self, score):
-        self.scores.append(score)
-
-    def get_scores(self):
-        return self.scores
 
 
 class GameSession:
